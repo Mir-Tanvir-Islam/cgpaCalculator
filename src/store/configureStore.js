401d7147e14252129bfbb3fb60676d2c419b5149
Mir Tanvir Islam <tanvir.islam04@northsouth.edu>
2019-05-20 01:24:48 +0600

    Redux store configured

---
+++ src/store/configureStore.js
@@ -0,0 +1,13 @@
+import {createStore, combineReducers} from 'redux'
+import 'gradesReducer' from '../reducers/gradesReducer'
+import 'infoReducer' from '../reducers/infoReducer'
+
+export default()=>{
+	const store =createStore(
+		combineReducers({
+			grades:gradesReducer,
+			info:infoReducer
+		})
+	)
+	return store
+}
